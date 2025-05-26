b93d285125305faa9ae2f2a6ab29792d43bbe30f
Thiago Gabriel <58753482+TryagoXD@users.noreply.github.com>
2025-05-26 14:46:41 -0300

    Add files via upload

---
+++ calculadora.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+touch resultado.txt
+
+echo "Bem-vindo a Calculadora shell"
+echo "Realizar operações com + - * e /"
+echo "Use () para priorizar alguma parte sobre a outra."
+
+echo "Instruções: "
+echo "Digite e para sair."
+echo "Digite s para começar e para continuar."
+echo "Digite r para usar o último resultado."
+
+while read -p "Escolha uma opção: " op
+do
+    if [ $op = 's' ]; then
+        read -p "Digite uma expressão: " n1
+        let result=$n1
+        echo $result > resultado.txt
+        temp=$(cat resultado.txt)
+        
+        echo "Resposta: $result"
+    elif [ $op = 'r' ]; then
+        read -p "$temp" n1
+        let result=$temp$n1
+        echo $result > resultado.txt
+        temp=$(cat resultado.txt)
+        
+        echo "Resposta: $result"
+    elif [ $op = "e" ] ; then
+        echo "Programa encerrado!"
+        break
+    else
+        echo "Erro, selecione uma opção existente."
+    fi
+done
